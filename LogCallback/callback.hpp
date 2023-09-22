c88c96c7ae041e708f8390921121b90a3c0dc88d
Jean-Francois Ndi <jeanfrancois.ndi@gmail.com>
2023-09-22 22:06:43 +0200

    Reformat the log_record in a custom way.

--- LogCallback/callback.hpp
+++ LogCallback/callback.hpp
@@ -1,16 +1,32 @@
 #pragma once
 
 #include "ace/streams.h"
-#include "ace/Log_Msg.h"
 #include "ace/Log_Msg_Callback.h"
 #include "ace/Log_Record.h"
+#include "ace/SString.h"
 
 class Callback : public ACE_Log_Msg_Callback
 {
 public:
     void log(ACE_Log_Record& log_record)
     {
-        log_record.print(ACE_TEXT(""), 0, cerr);
-        log_record.print(ACE_TEXT(""), ACE_Log_Msg::VERBOSE, cerr);
+        cerr << "Log Message Received:" << endl;
+
+        unsigned long msg_severity = log_record.type();
+        ACE_Log_Priority prio = static_cast<ACE_Log_Priority>(msg_severity);
+        const ACE_TCHAR* prio_name = ACE_Log_Record::priority_name(prio);
+        cerr << "\tType:        " << ACE_TEXT_ALWAYS_CHAR(prio_name) << endl;
+
+        cerr << "\tLength:      " << log_record.length() << endl;
+
+        const time_t epoch = log_record.time_stamp().sec();
+        cerr << "\tTime_Stamp:  " << ACE_TEXT_ALWAYS_CHAR(ctime(&epoch));
+        cerr.flush();
+
+        cerr << "\tPid:         " << log_record.pid() << endl;
+
+        ACE_CString data(">> ");
+        data += ACE_TEXT_ALWAYS_CHAR(log_record.msg_data());
+        cerr << "\tMsgData      " << data.c_str() << endl;
     }
 };
